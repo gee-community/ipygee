9e8706d7ec801377f75e509a302ebc068979d8c9
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-10-18 15:24:40 +0000

    test: test GEE connection

--- tests/test_ipygee.py
+++ tests/test_ipygee.py
@@ -1,4 +1,5 @@
 """Test the ipygee package."""
+import ee
 
 import ipygee
 
@@ -6,3 +7,8 @@ import ipygee
 def test_hello_world():
     """Hello world test."""
     assert ipygee.Hello().hello_world() == "hello world !"
+
+
+def test_gee_connection():
+    """Test the geeconnection is working."""
+    assert ee.Number(1).getInfo() == 1
